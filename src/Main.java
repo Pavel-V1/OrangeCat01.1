c705d4526d2c33025d1f82588fab1780ba1768c2
pavel-v1 <pavelvoronov009@gmail.com>
2025-11-06 02:03:35 +0300

    +

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
-        //bTree
+    public static void main(String[] args) throws FileNotFoundException {
+        File horror_movies = new File("C:\\Users\\User\\IdeaProjects\\GitHub\\OrangeCat01.1", "horror_movies.csv");
+        Scanner scanner = new Scanner(horror_movies);
+        String title = scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            int counter = 0;
+            for (int i = 0; i < str.length(); i++) {
+
+                if ("\"".equals(String.valueOf(str.charAt(i))) && counter == 0) {
+                    //
+                    counter++;
+                } else {
+                    //
+                    counter--;
+                }
+            }
+        }
+//        System.out.println(list);
     }
 }
