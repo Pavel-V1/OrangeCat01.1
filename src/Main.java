cb84008ad9e48aec4aa5376fc64f4760070396c5
Pavel-V1 <pavelvoronov009@gmail.com>
2025-11-06 13:08:38 +0300

    +

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File horror_movies = new File("C:\\Users\\User\\IdeaProjects\\GitHub\\OrangeCat01.1", "horror_movies.csv");
+//        File horror_movies = new File("C:\\Users\\User\\IdeaProjects\\GitHub\\OrangeCat01.1", "horror_movies.csv");
+        File horror_movies = new File("C:\\Users\\voronov_p_a\\IdeaProjects\\OrangeCat01.1", "horror_movies.csv");
         Scanner scanner = new Scanner(horror_movies);
-        String title = scanner.nextLine();
+//        String title = scanner.nextLine();
+//        while (scanner.hasNextLine()) {
+//            String str = scanner.nextLine();
+//            StringBuilder str_build = new StringBuilder();
+//            int counter = 0;
+//            for (int i = 0; i < str.length(); i++) {
+//                boolean equals = "\"".equals(String.valueOf(str.charAt(i)));
+//                if (equals && counter == 0) {
+//                    //
+//                    counter++;
+//                } else if (equals && counter == 1) {
+//                    //
+//                    counter--;
+//                } else {
+//
+//                }
+//            }
+//        }
+        List list = new List();
         while (scanner.hasNextLine()) {
-            String str = scanner.nextLine();
-            StringBuilder stringBuilder = new StringBuilder();
-            int counter = 0;
-            for (int i = 0; i < str.length(); i++) {
-
-                if ("\"".equals(String.valueOf(str.charAt(i))) && counter == 0) {
-                    //
-                    counter++;
-                } else {
-                    //
-                    counter--;
-                }
-            }
         }
-//        System.out.println(list);
-        //https://docs.google.com/document/u/0/d/1eCEbv62df8ggqUgevpAdTFwWTbgfHe79-WUBVP3x9q8/mobilebasic
+        System.out.println(list);
     }
 }
