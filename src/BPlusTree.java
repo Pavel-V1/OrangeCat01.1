1064b1af651178c0eadb908cec0e303e7d8f3ea3
Pavel-V1 <pavelvoronov009@gmail.com>
2025-11-13 18:11:21 +0300

    checkpoint

--- src/BPlusTree.java
+++ src/BPlusTree.java
@@ -42,4 +42,8 @@ public class BPlusTree {
             }
         }
     }
+
+    public int Lookup() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,41 @@ class Main {
         // по "id" лезть в словарь и доставать данные.
 
         BPlusTree bpTree = new BPlusTree();
+        for (int id : hashMap.keySet()) {
+            bpTree.add(id);
+        }
+
+    }
+
+    public ArrayList<String> getMovieFromRealiseDate(String smallestDate, String largestDate, BPlusTree bpTree) {
+        for (int i = 0; i < smallestDate.length(); i++) {
+            char j = smallestDate.charAt(i);
+            char k = largestDate.charAt(i);
+            if (j > k) {
+                String str = smallestDate;
+                smallestDate = largestDate;
+                largestDate = str;
+                break;
+            }
+        }
+        ArrayList<String> movies = new ArrayList<>();
+        //
+        return movies;
+    }
+
+    public ArrayList<String> getMovieFromVoteAverage(String lowestAvgVote, String highestAvgVote, BPlusTree bpTree) {
+        //
+        ArrayList<String> movies = new ArrayList<>();
+        //
+        return movies;
+    }
+
+    public ArrayList<String> getMovieFromBothParameters(String smallestDate, String largestDate,
+                                                        String lowestAvgVote, String highestAvgVote, BPlusTree bpTree) {
+        ArrayList<String> movie_array = getMovieFromRealiseDate(smallestDate, largestDate, bpTree);
+        // взять фильмы подходящие по дате
+        ArrayList<String> movies = new ArrayList<>();
+        // сюда передать из movie_array фильмы подходящие по оценке
+        return movies;
     }
 }
